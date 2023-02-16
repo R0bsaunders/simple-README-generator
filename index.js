29c4e2e5232d49df8117c41afcbc4962095699f1
Rob S <me@rob-saunders.co.uk>
2023-02-16 20:36:06 +0000

    changed to an import instead of require

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+import fs from 'node:fs'
+import util from 'node:util'
+// const path = require('path');
+// const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+import inquirer from 'inquirer';
+    inquirer.prompt([
+        {
+          type: 'input',
+          name: 'Title',
+          message: 'What is the title?',
+        },
+        {
+          type: 'input',
+          name: 'Description',
+          message: 'What is the description of your project?',
+        },
+        {
+          type: 'input',
+          name: 'Table Of Contents',
+          message: 'Yes or No?',
+        },
+        {
+          type: 'input',
+          name: 'Installation',
+          message: 'What are the installation instructions?',
+        },
+        {
+          type: 'input',
+          name: 'Usage',
+          message: 'What are the usage instructions?',
+        },
+        {
+          type: 'input',
+          name: 'license',
+          message: 'License Type',
+        },
+        {
+        type: 'input',
+        name: 'Contributors',
+        message: 'Who are the project contributors',
+        }
 
-];
+      ]).then((data)=> {
+    console.log(data);
+})
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -22,43 +61,3 @@ init();
 
 
 const writeFileAsync = util.promisify(fs.writeFile);
-
-const promptUser = () =>
-
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'Title',
-      message: 'What is the title?',
-    },
-    {
-      type: 'input',
-      name: 'Description',
-      message: 'What is the description of your project?',
-    },
-    {
-      type: 'input',
-      name: 'Table of contents',
-      message: 'Yes or No?',
-    },
-    {
-      type: 'input',
-      name: 'Installation',
-      message: 'What are the installation instructions?',
-    },
-    {
-      type: 'input',
-      name: 'Usage',
-      message: 'What are the usage instructions?',
-    },
-    {
-      type: 'input',
-      name: 'license',
-      message: 'License Type',
-    },
-    {
-        type: 'input',
-        name: 'Contributors',
-        message: 'Who are the project contributors',
-      },
-  ]);
