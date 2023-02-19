9026d9a198486964831d9ae4ef26c5b0d4300336
Rob S <me@rob-saunders.co.uk>
2023-02-19 10:49:30 +0000

    Following DRY principle, I have sussed out how to use a single function to validate each answer instead of having the same code in every choice

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-import fs from 'node:fs'
+// Importing File System
+import fs from 'node:fs';
+
+// Importing the function required to generate markdown
 import {generateMarkdown} from "./utils/generateMarkdown.js";
+
+// Importing inquirer package
 import inquirer from 'inquirer';
-import {choices} from "./utils/choices.js"
+
+// Importing the list of questions from external file
+import {choices} from "./utils/choices.js";
 
 // function call to initialize program
 init();
@@ -9,17 +16,18 @@ init();
 // function to initialize program
 function init() {
   
-  inquirer.prompt(choices).then((answers)=> {
+  inquirer.prompt(choices)
+  
+  .then((answers)=> {
 
-    writeToFile("README.md", generateMarkdown(answers))
+    writeToFile("README.md", generateMarkdown(answers));
 
   });
-
 };
 
 // function to write README file
 function writeToFile(fileName, data) {
 
-  fs.writeFile("README.md", data, (err) =>err ? console.log(err):console.log("Success! You have a FANTASTIC readme file!"))
+  fs.writeFile("README.md", data, (err) =>err ? console.log(err) : console.log("Success! You have a FANTASTIC readme file!"));
 
 };
--- utils/choices.js
+++ utils/choices.js
@@ -1,96 +1,94 @@
 // array of questions for user
 const choices = [
     {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title?',
-      validate: function(inputTitle) {
-        let title = inputTitle;
-  
-        return (!title ? 'Please enter a title' : true);
-      }
+        type: 'input',
+        name: 'title',
+        message: 'What is the title?',
+        validate: function(title) {
+
+            return validator(title);
+        }
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'What is the description of your project?',
-      validate: function(inputDescription) {
-        let description = inputDescription;
-  
-        return (!description ? 'Please enter a description' : true);
-      }
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+        validate: function(desc) {
+
+            return validator(desc);
+        }
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the installation instructions?',
-      validate: function(inputInstallation) {
-        let installation = inputInstallation;
-  
-        return (!installation ? 'Please enter installation instructions' : true);
-      }
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+        validate: function(inst) {
+
+        return validator(inst);
+        }
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'What are the usage instructions?',
-      validate: function(inputUsage) {
-        let usage = inputUsage;
-  
-        return (!usage ? 'Please enter installation instructions' : true);
-      }
+        type: 'input',
+        name: 'usage',
+        message: 'What are the usage instructions?',
+        validate: function(use) {
+
+            return validator(use);
+        }
     },
     {
-      type: 'list',
-      name: 'license',
-      message: 'Select a license Type',
-      choices:
+        type: 'list',
+        name: 'license',
+        message: 'Select a license Type',
+        choices:
         [
-          'Apache',
-          'Boost',
-          'MIT'
+            'Apache',
+            'Boost',
+            'MIT'
         ]
     },
     {
-    type: 'input',
-    name: 'contributing',
-    message: 'Who are the project contributors',
-    validate: function(inputContributors) {
-      let contributors = inputContributors;
-  
-      return (!contributors ? 'Please enter installation instructions' : true);
-    }
+        type: 'input',
+        name: 'contributing',
+        message: 'Who are the project contributors',
+        validate: function(contr) {
+
+            return validator(contr);
+        }
     },
     {
-      type: 'input',
-      name: 'tests',
-      message: 'What tests have been run?',
-      validate: function(inputTests) {
-        let tests = inputTests;
-  
-        return (!tests ? 'Please enter installation instructions' : true);
-      }
+        type: 'input',
+        name: 'tests',
+        message: 'What tests have been run?',
+        validate: function(test) {
+
+            return validator(test);
+        }
       },
       {
         type: 'input',
         name: 'github',
         message: 'What is your GitHub username?',
-        validate: function(inputGithub) {
-          let github = inputGithub;
-  
-          return (!github ? 'Please enter your GitHub username' : true);
+        validate: function(git) {
+
+            return validator(git);
         }
         },
-        {
-          type: 'input',
-          name: 'email',
-          message: 'What is your email address?',
-          validate: function(inputEmail) {
-            let email = inputEmail;
-  
-            return (!email ? 'Please enter your GitHub username' : true);
-          }
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: function(email) {
+
+            return validator(email);
         }
+    }
   ];
 
-  export {choices};
+// Function to check for empty answers and return message prompting user to type something
+function validator(input) {
+    return (!input ? 'Please enter a value' : true)
+
+};
+
+export {choices};
